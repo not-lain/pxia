d5c12dccdb5019c6003874911cfc34529a68a193
Lain <hhichri60@gmail.com>
2024-08-26 11:21:28 +0100

    add load from origin

--- src/pxia/gpt2.py
+++ src/pxia/gpt2.py
@@ -4,7 +4,7 @@ from torch import nn
 import math
 from torch.nn import functional as F
 from huggingface_hub import PyTorchModelHubMixin
-
+import inspect
 
 model_card_template = """
 ---
@@ -154,3 +154,41 @@ class GPT2(
         x = self.transformer.ln_f(x)
         x = self.lm_head(x)
         return x
+
+    @classmethod
+    def from_origin(cls,repo_id:str) : 
+        # load model weights from huggingface hub
+        try : 
+            from transformers import AutoModelForCausalLM
+        except ImportError : 
+            print("Please install transformers library to load model weights from huggingface hub")
+            return None
+        model_hf  = AutoModelForCausalLM.from_pretrained(repo_id)
+        params = inspect.getargspec(cls.__init__)[0]
+        kwargs = {k: getattr(model_hf.config, k, None) for k in params}
+        model = cls(**kwargs)
+        # sanitize state_dict
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard this mask / buffer, not a param
+        sd_hf = model_hf.state_dict()
+        # copy while ensuring all of the parameters are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these, just a buffer
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # same, just the mask (buffer)
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
+        # this means that we have to transpose these weights when we import them
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for the Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+        return model
