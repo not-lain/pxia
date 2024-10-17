68aec9e86d12b4f911cd3d07857217e22d8120e5
Lain <hhichri60@gmail.com>
2024-10-17 15:44:12 +0100

    update automodel

--- src/pxia/auto.py
+++ src/pxia/auto.py
@@ -1,18 +1,35 @@
 from .gpt2 import GPT2
 from .ModelingHannibal import Hannibal
-from huggingface_hub import model_info , PyTorchModelHubMixin
+from huggingface_hub import model_info, PyTorchModelHubMixin
+
+from functools import wraps
+
+
+def set_doc(doc):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        wrapper.__doc__ = doc
+        return wrapper
+
+    return decorator
+
 
 # Inspect repo parameters and return the appropriate model class
 
+
 class AutoModel:
     @classmethod
-    def from_pretrained(cls, repo_id, *model_args, **kwargs):
-        tags = model_info(repo_id).tags
+    @set_doc(PyTorchModelHubMixin.from_pretrained.__doc__)
+    def from_pretrained(cls, pretrained_model_name_or_path, *model_args, **kwargs):
+        f"{PyTorchModelHubMixin.from_pretrained.__doc__}"
+
+        tags = model_info(pretrained_model_name_or_path).tags
         if "gpt2" in tags:
-            return GPT2.from_pretrained(repo_id,*model_args,**kwargs)
+            return GPT2.from_pretrained(pretrained_model_name_or_path, *model_args, **kwargs)
         elif "hannibal" in tags:
-            return Hannibal.from_pretrained(repo_id,*model_args,**kwargs)
+            return Hannibal.from_pretrained(pretrained_model_name_or_path, *model_args, **kwargs)
         else:
             raise ValueError("this model is not part of pxia")
-
-AutoModel.from_pretrained.__doc__ = PyTorchModelHubMixin.from_pretrained.__doc__
